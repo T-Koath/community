f3e185a45fb992327c0dace8328f4d8bedfecfd5
T-Koath <576017461@qq.com>
2021-05-19 16:15:16 +0800

    添加新功能，回复，提问

---
+++ src/main/java/life/majiang/community/controller/ProfileController.java
@@ -0,0 +1,29 @@
+package life.majiang.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @ClassName ProfileController
+ * @Description TODO
+ * @date 2021/5/19 14:49
+ * @Version 1.0
+ */
+@Controller
+public class ProfileController {
+
+    @GetMapping("/profile/{action}")
+    public String profile(@PathVariable(name = "action")String action,Model model){
+        if ("questions".equals(action)){
+            model.addAttribute("section","questions");
+            model.addAttribute("sectionName","我的提问");
+        } else if ("replies".equals(action)){
+            model.addAttribute("section","replies");
+            model.addAttribute("sectionName","最新回复");
+        }
+
+        return "profile";
+    }
+}
